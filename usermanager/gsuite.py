bdc3ae6057d2f660e68d9283c536f30d19e2babb
Sergio B <sbestetti@gmail.com>
2018-10-19 16:00:33 +0100

    User search on G Suite

---
+++ usermanager/gsuite.py
@@ -0,0 +1,18 @@
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
+SERVICE_ACCOUNT_FILE = 'service.json'
+
+def searchUser(username):
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    delegated_credentials = credentials.with_subject('sergio.bestetti@distilled.ie')
+    service = googleapiclient.discovery.build('admin', 'directory_v1', credentials=delegated_credentials)
+    email = username + '@distilled.ie'
+    try:
+        results = service.users().get(userKey=email, projection='basic').execute()
+    except:
+        print('Google API error: {} not found'.format(username))
+        return None
+
+    return results 
