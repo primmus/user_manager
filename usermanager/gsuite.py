f7e4ff3f0856e1c29e4348cb9a3fc27e700c1564
Sergio B <sbestetti@gmail.com>
2018-10-21 07:45:52 +0100

    Treating different entries on username search

--- usermanager/gsuite.py
+++ usermanager/gsuite.py
@@ -8,6 +8,8 @@ def searchUser(username):
     credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     delegated_credentials = credentials.with_subject('sergio.bestetti@distilled.ie')
     service = googleapiclient.discovery.build('admin', 'directory_v1', credentials=delegated_credentials)
+    if '@' in username:
+        username = username.split('@')[0]
     email = username + '@distilled.ie'
     try:
         results = service.users().get(userKey=email, projection='basic').execute()
