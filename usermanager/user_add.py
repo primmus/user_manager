6f2e5d47b056b70eb780d6c74cead963135c36d3
Sergio B <sbestetti@gmail.com>
2018-10-25 13:15:36 +0100

    Add user form - 1st version

--- usermanager/user_add.py
+++ usermanager/user_add.py
@@ -5,7 +5,20 @@ bp = Blueprint('add', __name__, url_prefix='/add')
 
 @bp.route('/', methods=('POST', 'GET'))
 def addIndex():
-    
-        
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        userName = request.form['userName']
+        location = request.form['location']
+        aliases = request.form.getlist('aliases')
+        schibstedServices = request.form.getlist('schibstedServices')
+        services = request.form.getlist('services')
+        vpn = request.form.getlist('vpn')
+
+        print('Creating a new login {} for {} {}  in {} with the following access:'.format(userName, firstName, lastName, location))
+        print('Schibsted services: {}'.format(schibstedServices))
+        print('Corporate services: {}'.format(services))
+        print('VPN access: {}'.format(vpn))
+        print('Adding email aliases: {}'.format(aliases))     
 
     return render_template('add.html')
