f9bea7a72a9b9f7dd2f9b76863797f0a76379256
Sergio B <sbestetti@gmail.com>
2018-11-07 16:35:12 +0000

    Disable user basic page

--- usermanager/__main__.py
+++ usermanager/__main__.py
@@ -10,7 +10,10 @@ import blueprints.user_search as user_search
 app.register_blueprint(user_search.bp)
 
 import blueprints.user_add as user_add
-app.register_blueprint(user_add.bp)  
+app.register_blueprint(user_add.bp)
+
+import blueprints.user_disable as user_disable
+app.register_blueprint(user_disable.bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- usermanager/blueprints/user_disable.py
+++ usermanager/blueprints/user_disable.py
@@ -1,2 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, g
+import user, tools
 
+bp = Blueprint('disable', __name__, url_prefix='/disable')
+
+@bp.route('/', methods=('POST', 'GET'))
+def disableIndex():
+    userToDisable = user.User()
+    
+    if request.method == 'POST':                
+        username = request.form['username']
+        userToDisable = tools.getUser(username)        
+        
+        g.user = userToDisable        
+
+    return render_template('disable.html')
