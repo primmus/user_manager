c3cf2a40287ed39a5ef31d68ec2aabb037fc8ba4
Sergio B <sbestetti@gmail.com>
2018-11-08 11:05:45 +0000

    Addedd the Disable button to the end of the Disable user form

--- usermanager/blueprints/user_disable.py
+++ usermanager/blueprints/user_disable.py
@@ -6,11 +6,16 @@ bp = Blueprint('disable', __name__, url_prefix='/disable')
 @bp.route('/', methods=('POST', 'GET'))
 def disableIndex():
     userToDisable = user.User()
+    g.isSearch = True
     
     if request.method == 'POST':                
-        username = request.form['username']
-        userToDisable = tools.getUser(username)        
+        g.isSearch = False
+        if request.form['btn'] == 'Check':
+            username = request.form['username']
+            userToDisable = tools.getUser(username)        
         
-        g.user = userToDisable        
+            g.user = userToDisable
+        elif request.form['btn'] == 'Disable':
+            print('Disable user')
 
     return render_template('disable.html')
