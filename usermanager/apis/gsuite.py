5ee6b6d5cdcaf044a7117f976303f9ccf6587035
Sergio B <sbestetti@gmail.com>
2018-11-10 12:26:13 +0000

    gsuite.py: Implements GSuite user deactivation
    Current module now has a function capable of deactivating a given user
    on G Suite

--- usermanager/apis/gsuite.py
+++ usermanager/apis/gsuite.py
@@ -5,15 +5,25 @@ import sys
 
 SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
 SERVICE_ACCOUNT_FILE = 'service.json'
+# TODO: Move the user account to the settings files
+SUBJECT = 'sergio.bestetti@distilled.ie'
+
+def getService():
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    delegated_credentials = credentials.with_subject(SUBJECT)
+    return googleapiclient.discovery.build('admin', 'directory_v1', credentials=delegated_credentials)
+
+def disableUser(userToDisable):
+    service = getService()
+    myBody = {
+        "suspended": True
+    }
+    tmp = service.users().update(userKey=userToDisable.gMainEmail, body=myBody).execute()    
 
 def searchUser(userToSearch):        
-    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    delegated_credentials = credentials.with_subject('sergio.bestetti@distilled.ie')
-    service = googleapiclient.discovery.build('admin', 'directory_v1', credentials=delegated_credentials)    
-    
     try:        
-        results = service.users().get(userKey=userToSearch.gMainEmail, projection='basic').execute()
-        
+        service = getService()
+        results = service.users().get(userKey=userToSearch.gMainEmail, projection='basic').execute()        
         userToSearch.firstName = results['name']['givenName']
         userToSearch.lastName = results['name']['familyName']
         userToSearch.gMainEmail = results['primaryEmail']
--- usermanager/blueprints/user_disable.py
+++ usermanager/blueprints/user_disable.py
@@ -1,21 +1,25 @@
 from flask import Blueprint, render_template, request, g
-import user, tools
+import user, tools, apis
 
 bp = Blueprint('disable', __name__, url_prefix='/disable')
 
+userToDisable = user.User()
+
+
 @bp.route('/', methods=('POST', 'GET'))
 def disableIndex():
-    userToDisable = user.User()
     g.isSearch = True
     
     if request.method == 'POST':                
         g.isSearch = False
         if request.form['btn'] == 'Check':
             username = request.form['username']
+            global userToDisable
             userToDisable = tools.getUser(username)        
         
             g.user = userToDisable
         elif request.form['btn'] == 'Disable':
-            print('Disable user')
+            apis.gsuite.disableUser(userToDisable)
+
 
     return render_template('disable.html')
